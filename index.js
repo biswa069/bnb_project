05d19d9528182a232e9ae8939b9e7b244b1fc11c
Nayakyash005 <yashpankajkumarnayak@gmail.com>
2024-01-31 21:53:30 +0530

    xyz

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import axios from "axios";
-const port = 5000;
+const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -42,9 +42,7 @@ app.get("/about", async (req, res) => {
   res.render("about.ejs");
 });
 
-app.get("/sport", async (req, res) => {
-  db.query("SELECT * FROM sport");
-});
+
 const cred = [];
 app.post("/submit", (req, res) => {
   const { name, email, contact, location, message } = req.body;
@@ -76,31 +74,28 @@ app.get("/contact", async (req, res) => {
   });
 });
 
-app.get("/hackathon", async (req, res) => {
-  const result = await axios.get("https://devpost.com/api/hackathons?page=2");
-  hackathons = result.data.hackathons;
-  console.log(hackathons);
-  res.render("hackathon.ejs", {
-    posts: hackathons,
-  });
-});
+
 
 
 app.get("/hackathon",(async (req,res)=>{
-    const result = await axios.get("https://devpost.com/api/hackathons?page=2");
+    try {
+      const result = await axios.get("https://devpost.com/api/hackathons?page=2");
     hackathons = result.data.hackathons;
     console.log(hackathons);
     res.render("hackathon.ejs",{
         posts: hackathons,
     });
+    } catch (error) {
+      console.log(error.message);
+    }  
 }))
 app.listen(port,()=>{
     console.log(`app is live at http://localhost${port}`);
 })
-
+let Image = "/images/bg1.png";
 app.get("/:category", async (req, res) => {
   const category = req.params.category;
-
+    
 const url = "https://api.predicthq.com/v1/events/";
 
 const headers = {
@@ -109,22 +104,33 @@ const headers = {
 };
 
 let params = {
-  category:"sports",
-  country: "IN",
+  category,
+   country: "IN",
 
-  "location_around.origin": "20.267646,85.833995",
+"location_around.origin": "20.267646,85.833995",
+  
 };
 //    let params = {};
 
+if(category == 'concerts'){
+  Image = '/images/concerts.avif';
+}else if(category == 'festivals'){
+  Image = '/images/festival.avif';
+}
+
 try {
   const response = await axios.get(url, { headers, params });
-  res.json(response.data);
+  
+  res.render("category.ejs",{
+    posts: response.data.results,
+    Image: Image,
+  });
 } catch (error) {
   console.error(error);
   res.status(500).send("An error occurred while searching for events.");
 }
 });
-app.listen(port, () => {
+app.listen(8000, () => {
   console.log(`app is live at http://localhost${port}`);
 });
 
